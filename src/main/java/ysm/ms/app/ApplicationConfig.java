5a42175cbe9d0f496703386211ee383f2c904639
yajairasoler <yajairasoler@gmail.com>
2016-12-04 11:14:24 -0500

    Messages

---
+++ src/main/java/ysm/ms/app/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package ysm.ms.app;
+
+import javax.inject.Named;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+
+	@Named
+	static class JerseyConfig extends ResourceConfig {
+
+		public JerseyConfig() {
+
+			this.packages("ysm.ms.app.rest");
+		}
+
+	}
+
+}
---
+++ src/main/java/ysm/ms/app/rest/MessageService.java
@@ -0,0 +1,35 @@
+package ysm.ms.app.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Named;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Named
+@Path("/")
+public class MessageService {
+
+	private List<Message> messageList = new ArrayList<Message>();
+
+	public MessageService() {
+	}
+
+	@GET
+	@Path("addMessage")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(@QueryParam("message") String message,
+			@QueryParam("type") String type) {
+
+		// process the message
+		MessageType t = MessageType.valueOf(type);
+		Message mes = new Message(t, message);
+		return mes;
+
+	}
+
+}
